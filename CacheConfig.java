fe5727c0c19c83b58296f49c70bc70a14e702a58
sumit123194 <sumitsinghiter54321@gmail.com>
2025-08-26 13:54:04 +0530

    Add files via upload

---
+++ CacheConfig.java
@@ -0,0 +1,15 @@
+package com.example.Train.config;
+
+import com.example.Train.entity.TrainSchedule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+public class CacheConfig {
+//    @Bean(name="ttayhjh")
+//    public TrainSchedule getInstace(){
+//        return new TrainSchedule();
+//
+//    }
+}
